b0fe1bc5b1f1b57ef197d867b358f12ce380ce42
caobug <mingjian322@gmail.com>
2018-02-26 22:27:42 +0800

    support carthage

---
+++ Popover-iOS/Popover-iOS.h
@@ -0,0 +1,21 @@
+//
+//  Popover_iOS.h
+//  Popover-iOS
+//
+//  Created by caobug on 2018/2/26.
+//  Copyright © 2018年 lifution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Popover_iOS.
+FOUNDATION_EXPORT double Popover_iOSVersionNumber;
+
+//! Project version string for Popover_iOS.
+FOUNDATION_EXPORT const unsigned char Popover_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Popover_iOS/PublicHeader.h>
+
+#import <Popover/PopoverView.h>
+#import <Popover/PopoverAction.h>
+#import <Popover/PopoverViewCell.h>
